5ad83f2a05b7333e011de29337059c73f4fe62d4
Eric Oliver <ericoliver52@gmail.com>
2023-07-09 20:39:16 -0500

    I have drag and dropping working, done for th eday

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { Container } from "../components/container";
 import Header from "../components/header/header";
 import OrganizedList from "../components/organized-list/organized-list";
 import SideNav from "../components/side-nav/side-nav";
@@ -6,7 +7,7 @@ export default function Home() {
 	return (
 		<div>
 			<Header />
-			<OrganizedList></OrganizedList>
+			<Container />
 		</div>
 	)
 }
